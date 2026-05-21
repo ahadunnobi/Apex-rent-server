732e7dcd1096e386f4523b0ee6552f511596157d
ahadunnobi <hacker.ven10@gmail.com>
2026-05-21 06:08:10 +0600

    refactor: simplify CORS middleware configuration

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middlewares
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'], // Common frontend ports
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB URI
